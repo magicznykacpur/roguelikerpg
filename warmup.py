5897327d582c821adabc4f0e1cd7c5e87bc2797d
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-04-27 09:39:20 +0200

    testuję

---
+++ warmup.py
@@ -0,0 +1,95 @@
+
+
+def create_board(width, height):
+    board = []
+    outer_row = []
+    inner_row = []
+    width = int(width)
+    height = int(height)
+    for item in range(width):
+        outer_row.append('#')
+    inner_row.append('#')
+    for item in range(width - 2):
+        inner_row.append('-')
+    inner_row.append('#')
+    board.append(outer_row)
+    for item in range(height - 2):
+        board.append(inner_row[:])
+    board.append(outer_row)
+    return board
+
+
+def print_board(board):
+    board_list = board
+    for item in board_list:
+        print("".join(item))
+
+
+def insert_player(board, x, y):
+    x = int(x)
+    y = int(y)
+    karakan = "@"
+    board[y][x] = karakan
+    return board
+
+
+def main():
+    global board
+    global x
+    global y
+    width = input("Enter map width: ")
+    height = input("Enter map height: ")
+    x = input("Enter character x: ")
+    y = input("Enter character y: ")
+    board = create_board(width, height)
+    board = insert_player(board, x, y)
+    print_board(board)
+    
+    while True:
+        chuj = getch()
+        if chuj == "a":
+            x = int(x)
+            y = int(y)
+            board = insert_player(board, x-1, y)
+            board[y][x] = "-"
+            print_board(board)
+            x = x - 1
+        elif chuj == "d":
+            x = int(x)
+            y = int(y)
+            board = insert_player(board, x+1, y)
+            board[y][x] = "-"
+            print_board(board)
+            x = x + 1
+        elif chuj == "w":
+            x = int(x)
+            y = int(y)
+            board = insert_player(board, x, y-1)
+            board[y][x] = "-"
+            print_board(board)
+            y = y - 1
+        elif chuj == "s":
+            x = int(x)
+            y = int(y)
+            board = insert_player(board, x, y+1)
+            board[y][x] = "-"
+            print_board(board)
+            y = y + 1
+
+
+def getch():
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+if __name__ == '__main__':
+    main()
